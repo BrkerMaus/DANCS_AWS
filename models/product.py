63f11dbdc8c84c0f2f0052d77788d73be352386d
BrkerMaus <103913208+BrkerMaus@users.noreply.github.com>
2025-03-18 04:47:21 +0800

    Update product.py

--- models/product.py
+++ models/product.py
@@ -1,4 +1,5 @@
 from config import get_products_db_connection
+from models.user import log_action
 
 def get_products():
     connection = get_products_db_connection()
@@ -19,7 +20,7 @@ def get_products():
     connection.close()
     return products
 
-def add_product(name, category, quantity, price, status):
+def add_product(username, name, category, quantity, price, status):
     connection = get_products_db_connection()
     cursor = connection.cursor()
     
@@ -29,13 +30,15 @@ def add_product(name, category, quantity, price, status):
     """
     cursor.execute(query, (name, category, quantity, price, status))
     connection.commit()
-    
+
+    log_action(username, f"Added Product: {name}")
     connection.close()
     
-def edit_product(product_id, name, category, quantity, price):
+def edit_product(username, product_id, name, category, quantity, price):
     try:
         connection = get_products_db_connection()
         cursor = connection.cursor()
+
         if quantity > 20:
             status = "Available"
         elif 0 < quantity <= 20:
@@ -50,18 +53,21 @@ def edit_product(product_id, name, category, quantity, price):
         """
         cursor.execute(query, (name, category, quantity, price, status, product_id))
         connection.commit()
+
+        log_action(username, f"Edited Product ID: {product_id}")
         print("Product updated successfully")
     except Exception as e:
         print("Error updating product:", str(e))
     finally:
         connection.close()
 
-def delete_product(product_id):
+def delete_product(username, product_id):
     connection = get_products_db_connection()
     cursor = connection.cursor()
     
     query = "DELETE FROM products WHERE id = %s"
     cursor.execute(query, (product_id,))
     connection.commit()
-    
+
+    log_action(username, f"Deleted Product ID: {product_id}") 
     connection.close()
