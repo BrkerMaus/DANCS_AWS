9c8253c828371edaf3a4fae639a3b9168fa68f13
BrkerMaus <103913208+BrkerMaus@users.noreply.github.com>
2025-03-18 04:47:41 +0800

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from config import get_users_db_connection
+from datetime import datetime
 
 def get_users():
     connection = get_users_db_connection()
@@ -9,3 +10,24 @@ def get_users():
     
     connection.close()
     return users
+
+def create_user(username, password_hash):
+    connection = get_users_db_connection()
+    cursor = connection.cursor()
+    
+    query = "INSERT INTO users (username, password_hash) VALUES (%s, %s)"
+    cursor.execute(query, (username, password_hash))
+    connection.commit()
+    
+    log_action(username, "User Created")
+    connection.close()
+
+def log_action(username, action):
+    connection = get_users_db_connection()
+    cursor = connection.cursor()
+    
+    query = "INSERT INTO audit_logs (username, action, timestamp) VALUES (%s, %s, %s)"
+    cursor.execute(query, (username, action, datetime.now()))
+    connection.commit()
+    
+    connection.close()
